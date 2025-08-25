f30b06fe9503dacc2f713438615d65ab6637b485
olafoyeku-josiah <olafoyekujosiah@gmail.com>
2025-08-25 12:21:45 -0700

    extra files added and minor changes

--- main_web.py
+++ main_web.py
@@ -1,15 +1,31 @@
-from flask import flask, render_template, url_for
-from flask import sqlAlchemy
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
 
 
-app=flask(__name__)
+main_web=Flask(__name__)
+main_web.config['SQLALCHEMY_DATABASE_URL']='sqlite:///test.db'
 
-@app.route('/')
+db=SQLAlchemy(main_web)
+
+class Todo(db.model):
+    id= db.column(db.integer,primary_key=True)
+    content=db.column(db.string(200), nullable=False)
+    collected=db.column(db.integer, default=0)
+    date_created=db.column(db.integer, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<task %r>'  %self.id
+
+
+@main_web.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
 
 
 
 
 if __name__=="__main_web__":
-    app.run(debug=True)
+    with main_web.app_context():
+        db.create_all()
+    main_web.run(debug=True)
